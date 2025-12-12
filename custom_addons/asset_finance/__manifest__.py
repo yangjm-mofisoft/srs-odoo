d1dc2e97d1f89a4e349d633c13007b6620215676
Jiming Yang <yang_jiming@zooloo.asia>
2025-12-12 16:30:18 +0800

    Update view sequence in manifest file to fix module activation error

--- custom_addons/asset_finance/__manifest__.py
+++ custom_addons/asset_finance/__manifest__.py
@@ -9,12 +9,13 @@
         'security/security.xml',
         'security/ir.model.access.csv',
         'data/sequence.xml',
-        #'data/test_users_data.xml',  # For testing user setup. Remove in production.
+        'data/test_users_data.xml',  # For testing user setup. Remove in production.
         'data/cron.xml',
         'data/finance_term_data.xml',
         'data/account_chart_data.xml',
         'data/mail_templates.xml',
-        'views/access_rights_views.xml',
+
+        # --- 1. DEFINE ACTIONS FIRST ---
         'views/dashboard_views.xml',
         'views/asset_views.xml',
         'views/asset_sg_views.xml',
@@ -28,8 +29,14 @@
         'wizard/disbursement_wizard_views.xml',
         'views/account_payment_views.xml',
         'views/report_views.xml',
-        'views/menu_views.xml',
-        'views/res_config_settings_views.xml',
+
+        # --- 2. DEFINE MAIN MENU STRUCTURE ---
+        'views/menu_views.xml',  # Creates 'menu_finance_config'
+
+        # --- 3. ADD ITEMS TO THAT STRUCTURE ---
+        'views/access_rights_views.xml',       # Adds 'Security' to 'menu_finance_config'
+        'views/res_config_settings_views.xml', # Adds 'Settings' to 'menu_finance_config'
+        
         'reports/finance_reports.xml',
         'reports/finance_contract_template.xml',
     ],
